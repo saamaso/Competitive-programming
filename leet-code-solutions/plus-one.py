2bf9512938710b3a526e2fabb0003dc175efec53
saamaso <151218786+saamaso@users.noreply.github.com>
2023-11-18 21:00:30 +0300

    Added solution for Plus One

---
+++ leet-code-solutions/plus-one.py
@@ -0,0 +1,7 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        stringo =""
+        for n in digits:
+            stringo+= str(n)
+        stringo = str(int(stringo)+1)
+        return "".join(stringo)
